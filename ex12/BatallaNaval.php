410c491868db3a601332a317ec1c33b41a8b2c8c
Henry <henydoudoc@gmail.com>
2023-09-24 20:13:47 +0200

    Third version, falta vaixells

---
+++ ex12/BatallaNaval.php
@@ -0,0 +1,20 @@
+<?php
+$n = 10;
+$m = 10;
+$submari=array("submari"=>2);
+echo "<table border='1'>";
+echo "<tr><td></td>"; 
+for ($j = 1; $j <= $m; $j++) {
+    echo "<td>" . chr(64 + $j) . "</td>"; 
+}
+echo "</tr>";
+for ($i = 1; $i <= $n; $i++) {
+    echo "<tr>";
+    echo "<td>" . $i . "</td>";
+    for ($j = 1; $j <= $m; $j++) {
+        echo "<td></td>"; 
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
